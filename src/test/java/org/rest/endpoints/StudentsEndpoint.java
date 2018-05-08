de405a95aa350048c26d263bd4f39befe21a12ec
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-05-08 13:05:53 +0200

    Travis tool and Codecov added

--- src/test/java/org/rest/endpoints/StudentsEndpoint.java
+++ src/test/java/org/rest/endpoints/StudentsEndpoint.java
@@ -1,6 +1,13 @@
 package org.rest.endpoints;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 // TODO - write tests
 public class StudentsEndpoint {
-
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
 }
