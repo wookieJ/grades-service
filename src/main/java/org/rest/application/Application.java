5e92784dd0471f30da0568466c130b535b5ba910
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-06-09 17:28:51 +0200

    Heroku grizzly host and port

--- src/main/java/org/rest/application/Application.java
+++ src/main/java/org/rest/application/Application.java
@@ -5,6 +5,8 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.linking.DeclarativeLinkingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
 import java.net.URI;
 
 // TODO - authorization
@@ -16,6 +18,10 @@ public class Application {
      * URI where application started at.
      */
     public static final String BASE_URI = "http://localhost:8080";
+
+    private static URI getBaseURI(String hostname, int port) {
+        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
+    }
 
     /**
      * Creating new resources where JAX-RS can finds endpoints and returning GrizzlyServer basef on BASE_URI.
@@ -29,7 +35,16 @@ public class Application {
                 .packages("org.rest.endpoints");
         rc.register(org.rest.converters.DateParamConverterProvider.class);
         rc.register(org.rest.exceptions.AppExceptionMapper.class);
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+//        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+
+        String hostname = System.getenv("HOSTNAME");
+        if (hostname == null)
+            hostname = "localhost";
+        String port = System.getenv("PORT");
+        if (port == null)
+            port = "8080";
+
+        return GrizzlyHttpServerFactory.createHttpServer(getBaseURI(hostname,Integer.valueOf(port)), rc);
     }
 
     public static void main(String[] args) {
@@ -42,3 +57,34 @@ public class Application {
         }
     }
 }
+
+
+//public class Main {
+
+
+
+//    protected static HttpServer startServer(URI uri) throws IOException {
+//        System.out.println("Starting grizzly...");
+//        ResourceConfig rc = new PackagesResourceConfig("com.agile.spirit.openapi");
+//        return GrizzlyServerFactory.createHttpServer(uri, rc);
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+//
+//
+//        URI uri = getBaseURI(hostname, Integer.valueOf(port));
+//
+//        HttpServer httpServer = startServer(uri);
+//        System.out.println(String.format("Jersey app started with WADL available at "
+//                + "%sapplication.wadl\nHit enter to stop it...", uri, uri));
+//        if (isOnLocal) {
+//            System.in.read();
+//            httpServer.stop();
+//        } else {
+//            while (true) {
+//                System.in.read();
+//            }
+//        }
+//
+//    }
+//}
