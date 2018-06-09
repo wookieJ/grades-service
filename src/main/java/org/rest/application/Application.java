fc34375e2c16573a971bd85d9afab1f1a6eb830b
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-06-09 18:13:14 +0200

    Host and port generating fix

--- src/main/java/org/rest/application/Application.java
+++ src/main/java/org/rest/application/Application.java
@@ -13,17 +13,10 @@ import java.net.UnknownHostException;
 // TODO - write comments
 // TODO - add MANIFEST for running jar file
 public class Application {
-    private static String hostname;
-    private static int port;
-
     /**
      * URI where application started at.
      */
-    private static URI getBaseURI(String hostname, int port) {
-        URI uri = URI.create("http://" + hostname + ":" + port);
-        System.out.println(uri);
-        return uri;
-    }
+    private static final String BASE_URI = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
 
     /**
      * Creating new resources where JAX-RS can finds endpoints and returning GrizzlyServer based on base URI.
@@ -34,26 +27,13 @@ public class Application {
         ResourceConfig rc = new ResourceConfig().packages("org.glassfish.jersey.examples.linking").register(DeclarativeLinkingFeature.class).packages("org.rest.endpoints");
         rc.register(org.rest.converters.DateParamConverterProvider.class);
         rc.register(org.rest.exceptions.AppExceptionMapper.class);
-//        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-
-//        hostname = System.getenv("HOSTNAME");
-        hostname = "localhost";
-        if (hostname == null)
-            hostname = "localhost";
-
-        String portS = System.getenv("PORT");
-        if (portS == null)
-            portS = "8080";
-
-        port = Integer.valueOf(portS);
-        return GrizzlyHttpServerFactory.createHttpServer(getBaseURI(hostname, port), rc);
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
     public static void main(String[] args) {
         try {
             startServer();
-            System.out.println(String.format("Homework app started at http://" + hostname + ":" + port));
-            System.out.println("Example endpoint: http://" + hostname + ":" + port + "/courses");
+            System.out.println(String.format("Homework app started at " + BASE_URI));
         } catch (Exception e) {
             e.printStackTrace();
         }
