7e6b5f881c62b72458840cc308dc89dbc46d048a
wookieJ <lukaszjedryczka.biuro@gmail.com>
2019-10-12 08:03:50 +0200

    host and port test

--- src/main/java/org/rest/application/Application.java
+++ src/main/java/org/rest/application/Application.java
@@ -1,12 +1,11 @@
 package org.rest.application;
 
-import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.linking.DeclarativeLinkingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
-import java.net.UnknownHostException;
 
 // TODO - authorization
 // TODO - return empty list, not 404 response code
@@ -14,28 +13,33 @@ import java.net.UnknownHostException;
 // TODO - add MANIFEST for running jar file
 // TODO - throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity("Not found").build());
 public class Application {
+
+    private static final String HOST_NAME = "0.0.0.0";
+
     /**
      * URI where application started at.
      */
-    private static final String BASE_URI = "http://localhost:" + (System.getenv("PORT") != null ? System.getenv("PORT") : "8080") + "/";
+    private static URI getBaseURI(String hostname, int port) {
+        return UriBuilder.fromUri(hostname).port(port).build();
+    }
 
     /**
      * Creating new resources where JAX-RS can finds endpoints and returning GrizzlyServer based on base URI.
      *
      * @return GrizzlyServer from GrizzlyHttpServerFactory based on BASE_URI.
      */
-    public static HttpServer startServer() throws UnknownHostException {
-        ResourceConfig rc = new ResourceConfig().packages("org.glassfish.jersey.examples.linking").register(DeclarativeLinkingFeature.class).packages("org.rest.endpoints");
-        rc.register(org.rest.converters.DateParamConverterProvider.class);
-        rc.register(org.rest.exceptions.AppExceptionMapper.class);
-        rc.register(org.rest.config.CorsConfig.class);
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
-
     public static void main(String[] args) {
         try {
-            startServer();
-            System.out.println(String.format("Homework app started at " + BASE_URI));
+            ResourceConfig rc = new ResourceConfig()
+                    .packages("org.glassfish.jersey.examples.linking")
+                    .register(DeclarativeLinkingFeature.class).packages("org.rest.endpoints");
+            rc.register(org.rest.converters.DateParamConverterProvider.class);
+            rc.register(org.rest.exceptions.AppExceptionMapper.class);
+            rc.register(org.rest.config.CorsConfig.class);
+            int port = System.getenv("PORT") != null ? Integer.parseInt(System.getenv("PORT")) : 9998;
+            String host = "http://" + HOST_NAME + "/";
+            GrizzlyHttpServerFactory.createHttpServer(getBaseURI(host, port), rc);
+            System.out.println("Grizzly server runs at " + host + ":" + port);
         } catch (Exception e) {
             e.printStackTrace();
         }
